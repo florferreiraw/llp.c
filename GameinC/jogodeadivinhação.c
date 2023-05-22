6d8fc4727a4a560dd08ee2bfcf6aad40ad46e84e
Lorena Ferreira <127799245+florferreiraw@users.noreply.github.com>
2023-05-22 15:51:02 -0300

    Update jogodeadivinhação.c
    adicionado ao código um loop (inicialização, condição e incremento),  e break para parar o loop.

--- GameinC/jogodeadivinhação.c
+++ GameinC/jogodeadivinhação.c
@@ -4,20 +4,31 @@ int main() {
   printf("*****************************************\n");
   printf("* Bem-vindo(a) ao jogo de adivinhações! *\n");
   printf("*****************************************\n");
-  int numerosecreto = 42;
-  int chute;
+int numerosecreto = 42;
+int chute;
+  
+for(int i = 1; i<=3; i++) {
   printf("Qual é o seu chute?");
   scanf("%d", &chute);
   printf("Você chutou o número %d\n", chute);
-  if (chute == numerosecreto) {
-    printf("Parabéns, você acertou!\n");
-    printf("Você é um bom jogador!\n");
+  
+int acertou = chute == numerosecreto;
+  
+ if (acertou) {
+   printf("Parabéns, você acertou!\n");
+   printf("Você é um bom jogador!\n");
+   break;
   } else {
-    if (chute > numerosecreto) {
-      printf("O seu chute foi maior que o número secreto!\n");
+int maior = chute > numerosecreto;
+ if(maior) {   
+   printf("O seu chute foi maior que o número secreto!\n");
     }
-    if (chute < numerosecreto) {
-      printf("O seu chute foi menor que o número secreto!\n");
+  else {
+   printf("O seu chute foi menor que o número secreto!\n");
     }
   }
 }
+  printf("Fim de jogo!");
+}
+
+   
