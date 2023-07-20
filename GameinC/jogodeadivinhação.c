0608dde2d6c8f06a2a3730083388c5fdef669500
Lorena Ferreira <127799245+florferreiraw@users.noreply.github.com>
2023-07-20 14:27:32 -0300

    Update jogodeadivinhação.c

--- GameinC/jogodeadivinhação.c
+++ GameinC/jogodeadivinhação.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define NUMERO_DE_TENTATIVAS 3
 int main() {
   printf("*****************************************\n");
   printf("* Bem-vindo(a) ao jogo de adivinhações! *\n");
@@ -7,28 +7,24 @@ int main() {
 int numerosecreto = 42;
 int chute;
   
-for(int i = 1; i<=3; i++) {
+for(int i = 1; i<=NUMERO_DE_TENTATIVAS; i++) {
   printf("Qual é o seu chute?\n");
   scanf("%d", &chute);
   printf("Você chutou o número %d\n", chute);
   
 int acertou = chute == numerosecreto;
+int maior = chute > numerosecreto;
   
  if (acertou) {
    printf("Parabéns, você acertou!\n");
    printf("Você é um bom jogador!\n");
    break;
-  } else {
-int maior = chute > numerosecreto;
- if(maior) {   
+  } else if(maior) {  
    printf("O seu chute foi maior que o número secreto!\n");
-    }
-  else {
+ } else {
    printf("O seu chute foi menor que o número secreto!\n");
     }
   }
-}
-  printf("Fim de jogo!");
+    printf("Fim de jogo!");
 }
 
-   
